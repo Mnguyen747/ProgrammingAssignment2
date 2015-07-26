4cda71ee63dbdede4d5b82c78a33cf381527b7d1
Jimmy Nguyen <Mnguyen747@gmail.com>
2015-07-25 22:37:39 -0700

    hw 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix
+#------------------------
+#The makeCasheMatrix simply takes any invertible matrix as its argument.
+#It then stores the inverse of that matrix in the global environment. 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(y) {
+  inv_mat <<- solve(y)         #compute inverse
 }
 
 
-## Write a short comment describing this function
+#cacheSolve
+#------------------------
+#Assuming you have already run the makeCacheMatrix function (which means you would have 
+#two objects in the global environment: the matrix y, and its inverse), cacheSolve
+#does one of two things:
+# 1)If the inputed matrix x is 
+#   a).of the same dimensions as y
+#   b).contains the same values as y
+#   Then simply take the cached inverted value inv_mat from the makeCacheMatrix function.
+# 2)If either condition a or b from step 1 above is violated, compute the inverse of 
+#   matrix x and store it as the new inv_mat value. Also, store x in y. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+  if(sum(dim(y) - dim(x))==0){ #dimension comparison
+    if(sum(x-y)!=0){           #value comparison
+      inv_mat <<- solve(x)     #compute inverse
+      y <<- x                  #store x in y
+      inv_mat                  #print inverse
+    } else {
+      inv_mat                  #print inverse
+    }
+  } else {
+    inv_mat <<- solve(x)       #compute inverse
+    y <<- x                    #store x in y
+    inv_mat                    #print inverse
+  }
 }
